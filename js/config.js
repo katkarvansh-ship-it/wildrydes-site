bdfeb041238acf306a368564f335edcf7be63259
Vansh Katkar <katkarvansh@gmail.com>
2026-09-08 12:14:24 +0530

    Update AWS Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_1YK48lQiL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'g9e5ud39fhgqmiov35t858td', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
